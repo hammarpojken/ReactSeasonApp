446e015897951b2a1d1b4ede4db69c26ed55b6b0
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-17 16:40:21 +0100

    Displaying latitude using Geolocation

--- src/index.js
+++ src/index.js
@@ -2,8 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      errMsg: ""
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude
+        });
+      },
+      err => {
+        this.setState({
+          errMsg: err.message
+        });
+      }
+    );
+  }
   render() {
-    return <div>Latitude: </div>;
+    if (this.state.errMsg && !this.state.latitude) {
+      return <div>Error: {this.state.errMsg}</div>;
+    }
+    if (!this.state.errMsg && this.state.latitude) {
+      return <div>Latitude: {this.state.latitude}</div>;
+    }
+    return <div>Loading</div>;
   }
 }
 
